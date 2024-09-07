3958f7edd639a94643b610f47d04ef5cc2d1e319
MagaDaur <magarulit2014@gmail.com>
2024-09-07 15:14:14 +0300

    123

--- diar/fastapi_app.py
+++ diar/fastapi_app.py
@@ -1,5 +1,6 @@
-from typing import List
-import shutil
+from os import getenv
+from dotenv import load_dotenv
+
 import torch
 from pydub import AudioSegment
 from concurrent.futures import ThreadPoolExecutor
@@ -13,28 +14,34 @@ import pika
 import base64
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+load_dotenv()
 
-# Токен для Hugging Face
-HF_TOKEN = "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"
+HF_TOKEN = getenv('HF_TOKEN')
 
-# Устройство (CPU или GPU)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+credentials = pika.PlainCredentials('user', 'password')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='rabbitmq-server', credentials=credentials, heartbeat=1800))
+channel = connection.channel()
+
+channel.queue_declare(queue='auto_analyze')
+channel.queue_declare(queue='manual_analyze')
+channel.queue_declare(queue='telegram_text_upload')
+
+
 def transcribe_audio(audio_file, model_name="large-v2", compute_type="float16"):
-    """Транскрибация аудиофайла"""
     model = whisperx.load_model(model_name, device='cuda', compute_type=compute_type)
     transcription_result = model.transcribe(audio_file)
     return transcription_result
 
+
 def extract_audio_segments(audio_file, diarize_df):
-    """Извлечение аудиосегментов для каждого спикера"""
     audio = AudioSegment.from_file(audio_file)
     segments = []
 
     for _, row in diarize_df.iterrows():
-        start = row['start'] * 1000  # переводим в миллисекунды
+        start = row['start'] * 1000
         end = row['end'] * 1000
         segment = audio[start:end]
         segments.append(segment)
@@ -42,19 +49,22 @@ def extract_audio_segments(audio_file, diarize_df):
     diarize_df['audio_segment'] = segments
     return diarize_df
 
+
 def perform_diarization(audio_file):
-    """Диаризация аудиофайла"""
     diarization_pipeline = DiarizationPipeline(use_auth_token=HF_TOKEN, device='cuda')
     diarized = diarization_pipeline(audio_file)
     return diarized
 
+
 def perform_verification(longest_segments, reference_samples, reference_names):
-    """Сравнение каждого спикера с эталонными голосами и присвоение имен"""
-    verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="pretrained_models/spkrec-ecapa-voxceleb")
+    verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb",
+                                                   savedir="pretrained_models/spkrec-ecapa-voxceleb")
 
     results = {}
     with ThreadPoolExecutor() as executor:
-        future_to_speaker = {executor.submit(compare_audio_with_references, row, reference_samples, verification): speaker for speaker, row in longest_segments.iterrows()}
+        future_to_speaker = {
+            executor.submit(compare_audio_with_references, row, reference_samples, verification): speaker for
+            speaker, row in longest_segments.iterrows()}
         for future in future_to_speaker:
             speaker, comparison_results = future.result()
             results[speaker] = comparison_results
@@ -80,8 +90,8 @@ def perform_verification(longest_segments, reference_samples, reference_names):
 
     return matching
 
+
 def compare_audio_with_references(segment_row, reference_samples, verification):
-    """Сравнение одного сегмента с эталонными голосами"""
     audio_segment = segment_row['audio_segment']
     comparison_results = []
     for reference in reference_samples:
@@ -95,8 +105,8 @@ def compare_audio_with_references(segment_row, reference_samples, verification):
             comparison_results.append(0)
     return segment_row['speaker'], comparison_results
 
+
 def align_speakers_to_text(script, diarize_df, audio_file):
-    """Выравнивание текста по спикерам"""
     model_a, metadata = load_align_model(language_code=script["language"], device=DEVICE)
     script_aligned = align(script["segments"], model_a, metadata, audio_file, DEVICE)
 
@@ -116,34 +126,32 @@ def align_speakers_to_text(script, diarize_df, audio_file):
 
     return transcribed_text
 
+
 def generate_srt(transcribed_text, output_file="output.srt"):
-    """Генерация SRT файла из транскрибированного текста с указанием имен спикеров"""
     with open(output_file, "w", encoding="utf-8") as srt_file:
         for i, segment in enumerate(transcribed_text, start=1):
             start_time = segment['start']
             end_time = segment['end']
             text = segment['text']
             speaker = segment['speaker']
-            
-            # Формат времени для SRT (HH:MM:SS,ms)
-            start_time_srt = time.strftime('%H:%M:%S,', time.gmtime(start_time)) + f'{int((start_time * 1000) % 1000):03d}'
+
+            start_time_srt = time.strftime('%H:%M:%S,',
+                                           time.gmtime(start_time)) + f'{int((start_time * 1000) % 1000):03d}'
             end_time_srt = time.strftime('%H:%M:%S,', time.gmtime(end_time)) + f'{int((end_time * 1000) % 1000):03d}'
-            
-            # Запись сегмента в файл
+
             srt_file.write(f"{i}\n")
             srt_file.write(f"{start_time_srt} --> {end_time_srt}\n")
             srt_file.write(f"{speaker}: {text}\n\n")
-    
+
     print(f"SRT файл успешно создан: {output_file}")
 
 
 def count_unique_speakers(diarize_df):
-    """Подсчёт уникальных спикеров в результатах диаризации"""
     unique_speakers = diarize_df['speaker'].nunique()
     return unique_speakers
 
+
 def main(audio_file, reference_samples, reference_names):
-    """Основная функция анализа аудиофайла"""
     start_time = time.time()
 
     script = transcribe_audio(audio_file)
@@ -168,24 +176,19 @@ def main(audio_file, reference_samples, reference_names):
 
     return result
 
+
 def get_file_bytes_as_b64(file_path: str):
     with open(file_path, 'rb') as file:
-        return base64.b64encode(file.read()).decode() 
+        return base64.b64encode(file.read()).decode()
 
-'''
-    audio_file: UploadFile = File(...),
-    reference_files: List[UploadFile] = File(...),
-    '''
 
 def verify_audio(ch, method, properties, body):
     input_data = json.loads(body)
 
-    # Сохранение загруженного аудиофайла
     audio_file_path = f"/tmp/{input_data['audio']['filename']}"
     with open(audio_file_path, "wb") as buffer:
         buffer.write(base64.b64decode(input_data['audio']['buffer']))
 
-    # Сохранение эталонных файлов
     reference_names = []
     reference_file_paths = []
     for ref_file in input_data['speakers']:
@@ -195,59 +198,50 @@ def verify_audio(ch, method, properties, body):
             buffer.write(base64.b64decode(ref_file['buffer']))
         reference_file_paths.append(ref_file_path)
 
-    # Вызов основной функции
     result = main(audio_file_path, reference_file_paths, reference_names)
-    
-    # Генерация SRT файла
+
     output_srt = f"/tmp/{input_data['audio']['filename']}.srt"
     generate_srt(result['transcription'], output_file=output_srt)
-    
-    # Подсчёт уникальных спикеров
+
     unique_speakers = count_unique_speakers(pd.DataFrame(result['transcription']))
 
-    data = {'chat_id': input_data['chat_id'],"unique_speakers": unique_speakers, "srt_file": get_file_bytes_as_b64(output_srt)}
+    data = {'chat_id': input_data['chat_id'], "unique_speakers": unique_speakers,
+            "srt_file": get_file_bytes_as_b64(output_srt)}
 
     channel.basic_publish('', 'asr_to_llm', json.dumps(data))
-
 
 def process_audio(ch, method, properties, body):
     input_data = json.loads(body)
 
     file_location = f"./temp_{input_data['audio']['filename']}"
-    
+
     with open(file_location, "wb+") as file_object:
         file_object.write(base64.b64decode(input_data['audio']['buffer']))
 
-    # Транскрибация
     script = transcribe_audio(file_location)
 
-    # Диаризация
     diarize_df = perform_diarization(file_location)
 
-    # Выравнивание текста
     transcribed_text = align_speakers_to_text(script, diarize_df, file_location)
 
-    # Присвоение имен спикерам в тексте
     unique_speakers = count_unique_speakers(diarize_df)
-    speaker_mapping = {f"speaker{i+1}": f"speaker{i+1}" for i in range(unique_speakers)}
-    
+    speaker_mapping = {f"speaker{i + 1}": f"speaker{i + 1}" for i in range(unique_speakers)}
+
     for segment in transcribed_text:
         speaker_id = segment["speaker"]
         segment["speaker"] = speaker_mapping.get(speaker_id, f"Speaker {speaker_id}")
 
-    # Генерация SRT файла
     output_srt = f"./{input_data['audio']['filename']}.srt"
     generate_srt(transcribed_text, output_file=output_srt)
 
-    data = {'chat_id': input_data['chat_id'], "unique_speakers": unique_speakers, "srt_file": get_file_bytes_as_b64(output_srt)}
+    data = {'chat_id': input_data['chat_id'], "unique_speakers": unique_speakers,
+            "srt_file": get_file_bytes_as_b64(output_srt)}
 
-    channel.basic_publish('', 'asr_line', json.dumps(data))
+    channel.basic_publish('', 'telegram_text_upload', json.dumps(data))
 
-channel.queue_declare(queue='auto_analyze')
-channel.queue_declare(queue='manual_analyze')
-channel.queue_declare(queue='asr_line')
 
-channel.basic_consume(queue='auto_analyze', auto_ack=True, on_message_callback=verify_audio)
-channel.basic_consume(queue='manual_analyze', auto_ack=True, on_message_callback=process_audio)
+if __name__ == "__main__":
+    channel.basic_consume(queue='auto_analyze', auto_ack=True, on_message_callback=verify_audio)
+    channel.basic_consume(queue='manual_analyze', auto_ack=True, on_message_callback=process_audio)
 
-channel.start_consuming()
+    channel.start_consuming()
