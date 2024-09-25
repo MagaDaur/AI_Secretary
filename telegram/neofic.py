b8f62246d5afafac532ba01531fdffcac4ddd4ad
Gordeev.AI <tema.gor17@gmail.com>
2024-09-25 14:07:51 +0300

    123 dota 2

--- telegram/neofic.py
+++ telegram/neofic.py
@@ -1,6 +1,7 @@
 from fpdf import FPDF
 import datetime
 import PyPDF2
+from sqlalchemy.testing.plugin.plugin_base import logging
 
 from local_lib import (
     get_chat_metadata,
@@ -10,47 +11,53 @@ from local_lib import (
 from pydub import (
     AudioSegment
 )
+import logging
 
-def word(a:str):
-    if a=="speakers":
+logging.basicConfig(level=logging.INFO)
+
+
+def word(a: str):
+    if a == "speakers":
         return "Докладчики:"
-    elif a=="date":
+    elif a == "date":
         return "Срок:"
-    elif a=="otv":
+    elif a == "otv":
         return "Ответственный:"
-    elif a=="obraz":
+    elif a == "obraz":
         return "Образ результата:"
-    elif a=="context":
+    elif a == "context":
         return "Контекст обсуждения:"
-    elif a=="time":
+    elif a == "time":
         return "Время:"
     else:
         return ""
 
-goal="декларация цели встречи"
+
+goal = "декларация цели встречи"
 
 months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
-           'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+          'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
 
-voc={1:"I",2:"II",3:"III",4:"IV",5:"V",
-     6:"VI",7:"VII",8:"VIII",9:"IX",10:"X",
-     11:"XI",12:"XII",13:"XIII",14:"XIV",15:"XV",
-     16:"XVI",17:"XVII",18:"XVIII",19:"XIX",20:"XX"}
+voc = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V",
+       6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
+       11: "XI", 12: "XII", 13: "XIII", 14: "XIV", 15: "XV",
+       16: "XVI", 17: "XVII", 18: "XVIII", 19: "XIX", 20: "XX"}
+
 
 def create_pdf(data):
-    date=datetime.datetime.now()
-    cur=str(date.day)+"."+str(date.month)+"."+str(date.year)
+    date = datetime.datetime.now()
+    cur = str(date.day) + "." + str(date.month) + "." + str(date.year)
 
     metadata = get_chat_metadata(data['chat_id'])
     llm_reply = data['transcribed_text']
-    
-    sound : AudioSegment = AudioSegment.from_file(f'./temp/{metadata['chat_id']}/{metadata['audio']['filename']}')
+
+    sound: AudioSegment = AudioSegment.from_file(f'./temp/{metadata['chat_id']}/{metadata['audio']['filename']}')
 
     resume = {}
     for i in range(len(llm_reply)):
         for j in range(len(llm_reply[i])):
             question = llm_reply[i][j]['Вопрос обсуждения']
-            resume[f'{voc[j+1]}.{question}'] = {
+            resume[f'{voc[j + 1]}.{question}'] = {
                 'b': {
                     'speakers': llm_reply[i][j]['Участники обсуждения'],
                     'decision': llm_reply[i][j]['Принятое решение'],
@@ -59,11 +66,10 @@ def create_pdf(data):
                 }
             }
 
-
     dur = seconds_to_time(int(sound.duration_seconds))
     members = ", ".join(metadata['members'])
 
-    pdf=FPDF()
+    pdf = FPDF()
     pdf.add_page()
 
     pdf.add_font("DejaVu", "", "./fonts/DejaVuSans.ttf", uni=True)
@@ -78,16 +84,16 @@ def create_pdf(data):
     pdf.cell(200, 10, txt="ПРОТОКОЛ ВСТРЕЧИ", ln=1, align="L")
     pdf.multi_cell(0, line_height)
     pdf.set_font('DejaVu', "", 12)
-    pdf.cell(200, 10, txt="ДАТА:        "+cur, ln=1, align="L")
-    pdf.cell(200, 10, txt="ВРЕМЯ:        "+str(date.hour)+":"+str(date.minute), ln=1, align="L")
-    pdf.cell(200, 10, txt="ДЛИТЕЛЬНОСТЬ:        "+dur, ln=1, align="L")
-    pdf.cell(200, 10, txt="УЧАСТНИКИ:        "+members, ln=1, align="L")
+    pdf.cell(200, 10, txt="ДАТА:        " + cur, ln=1, align="L")
+    pdf.cell(200, 10, txt="ВРЕМЯ:        " + str(date.hour) + ":" + str(date.minute), ln=1, align="L")
+    pdf.cell(200, 10, txt="ДЛИТЕЛЬНОСТЬ:        " + dur, ln=1, align="L")
+    pdf.cell(200, 10, txt="УЧАСТНИКИ:        " + members, ln=1, align="L")
     pdf.multi_cell(0, line_height)
     pdf.set_font('DejaVu', "", 14)
     pdf.cell(200, 10, txt="ПОВЕСТКА ДНЯ", ln=1, align="L")
     pdf.multi_cell(0, line_height)
     pdf.set_font('DejaVu', "", 12)
-    pdf.cell(200, 10, txt="Цель встречи:"+goal, ln=1, align="L")
+    pdf.cell(200, 10, txt="Цель встречи:" + goal, ln=1, align="L")
     pdf.multi_cell(0, line_height)
     for i in resume:
         pdf.cell(200, 10, txt=i, ln=1, align="L")
@@ -99,13 +105,14 @@ def create_pdf(data):
     for i in resume:
         pdf.cell(200, 10, txt=i, ln=1, align="L")
         for j in resume[i]:
-            pdf.cell(200, 10, txt=j+")", ln=1, align="L")
+            pdf.cell(200, 10, txt=j + ")", ln=1, align="L")
             for k in resume[i][j]:
-                if k=="speakers":
-                    pdf.cell(200, 10, txt=word(k)+', '.join(resume[i][j][k]), ln=1, align="L")
+                if k == "speakers":
+                    pdf.cell(200, 10, txt=word(k) + ', '.join(resume[i][j][k]), ln=1, align="L")
                 else:
-                    pdf.multi_cell(200, 10, txt=word(k)+resume[i][j][k], align="L")
-    
+                    logging.info(f"{word(k)} \n {resume[i][j][k]}")
+                    pdf.multi_cell(200, 10, txt=word(k) + (resume[i][j][k] or " "), align="L")
+
     filename = ''.join(data["file_name"].split(".")[:-1])
     pdf_filepath = f'./temp/{data["chat_id"]}/{filename}.pdf'
 
@@ -117,7 +124,7 @@ def create_pdf(data):
             for page in pdf_reader.pages:
                 pdf_writer.add_page(page)
             pdf_writer.encrypt(user_password=metadata['password'], owner_pwd=metadata['password'])
-            
+
             pdf_writer.write(pdf_file)
-    
+
     return pdf_filepath
